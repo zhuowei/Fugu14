217feec9171b412b861ae7aca4ce0a3aee9d364a
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2022-04-30 16:41:17 -0400

    KernelExploit: load and rebase kernel from copy on disk instead of reading from memory

--- arm/iOS/jailbreakd/build.sh
+++ arm/iOS/jailbreakd/build.sh
@@ -2,7 +2,7 @@ set -e
 
 CODESIGN_IDENTITY="4C1ED5BB460E754D8615D5D180BEB42AC96E1C15"
 
-swiftcArgs=(-sdk "`xcrun --sdk iphoneos --show-sdk-path`" -target arm64-apple-ios14.0 -O -framework IOKit)
+swiftcArgs=(-sdk "`xcrun --sdk iphoneos --show-sdk-path`" -target arm64-apple-ios14.0 -O -framework IOKit -g)
 
 swiftBuild=(swift build -c release -Xcc "-DIOS_BUILD" -Xcc -target -Xcc arm64-apple-ios14.0 -Xcc -isysroot -Xcc "`xcrun --sdk iphoneos --show-sdk-path`" -Xlinker "$PWD/libkernrw_linking/libkernrw.0.tbd")
 for arg in ${swiftcArgs[*]}
