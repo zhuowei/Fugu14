d69ad692f24edf77fa6df554b5f19cafe48bb28c
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2022-04-30 11:02:35 -0400

    KernelExploit: link libkernrw

--- arm/iOS/jailbreakd/build.sh
+++ arm/iOS/jailbreakd/build.sh
@@ -4,7 +4,7 @@ CODESIGN_IDENTITY="4C1ED5BB460E754D8615D5D180BEB42AC96E1C15"
 
 swiftcArgs=(-sdk "`xcrun --sdk iphoneos --show-sdk-path`" -target arm64-apple-ios14.0 -O -framework IOKit)
 
-swiftBuild=(swift build -c release -Xcc "-DIOS_BUILD" -Xcc -target -Xcc arm64-apple-ios14.0 -Xcc -isysroot -Xcc "`xcrun --sdk iphoneos --show-sdk-path`")
+swiftBuild=(swift build -c release -Xcc "-DIOS_BUILD" -Xcc -target -Xcc arm64-apple-ios14.0 -Xcc -isysroot -Xcc "`xcrun --sdk iphoneos --show-sdk-path`" -Xlinker "$PWD/libkernrw_linking/libkernrw.0.tbd")
 for arg in ${swiftcArgs[*]}
 do
     swiftBuild+=(-Xswiftc "$arg")
